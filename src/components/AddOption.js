2e67137f5d3a142a5b489625743de3408be32ad1
ksamy90 <ksamy90@aol.com>
2019-07-02 23:24:23 -0700

    Add styles for Options list

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -17,12 +17,13 @@ export default class AddOption extends React.Component {
     render() {
         return (
             <div>
-                {this.state.error && <p>{this.state.error}</p>}
+                {this.state.error && <p className="add-option-error">{this.state.error}</p>}
                 <form 
+                    className="add-option"
                     autoComplete="off" 
                     onSubmit={this.handleAddOption}
                 >
-                    <input type="text" name="option" />
+                    <input className="add-option__input" type="text" name="option" />
                     <button className="button">Add Option</button>
                 </form>
             </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -13,9 +13,10 @@ const Options = props => (
             </button>
         </div>
         {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
-        {props.options.map(option => (
+        {props.options.map((option, index) => (
             <Option 
-                key={option} 
+                key={option}
+                number={index + 1} 
                 optionText={option} 
                 handleDeleteOption={props.handleDeleteOption}
             />
