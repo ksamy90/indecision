8511f5ef30b87aed3df8fe7679619a4f745890f0
Sammy <ksamy90@aol.com>
2021-08-31 17:54:48 +0300

    indecision app

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -1,8 +1,64 @@
 import React from "react";
+import AddOption from "./AddOption";
+import Action from "./Action";
+import Header from "./Header";
+import Options from "./Options";
+import OptionModal from "./OptionModal";
 
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleRemoveOption = this.handleRemoveOption.bind(this);
+    this.handleClearOption = this.handleClearOption.bind(this);
+    this.state = {
+      options: [],
+      selectedOption: undefined,
+    };
+  }
+  handleClearOption() {
+    this.setState(() => {
+      return {
+        selectedOption: undefined,
+      };
+    });
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "this option already exists";
+    }
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat(option),
+      };
+    });
+  }
+  handleRemoveAll() {
+    this.setState(() => {
+      return {
+        options: [],
+      };
+    });
+  }
+  handleRemoveOption(itemToRemove) {
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.filter((option) => itemToRemove !== option),
+      };
+    });
+  }
+  handlePick() {
+    const randomNumber = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNumber];
+    this.setState(() => {
+      return {
+        selectedOption: option,
+      };
+    });
   }
   render() {
     return (
